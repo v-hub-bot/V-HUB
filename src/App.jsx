b6e0bdfecd0a92254414a50d45ab10bebe398d85
Eugene Cook Jr <kimberlycook1980@gmail.com>
2026-04-19 19:02:14 +0000

    Update app pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Home from './pages/Home';
+import Wekcadmin from './pages/Wekcadmin';
+import Classifieds from './pages/Classifieds';
 import ProviderDashboard from './pages/ProviderDashboard';
 import Terms from './pages/Terms';
-import Classifieds from './pages/Classifieds';
-import Wekcadmin from './pages/Wekcadmin';
-import ListService from './pages/ListService';
 import Privacy from './pages/Privacy';
+import ListService from './pages/ListService';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/Home" replace />} />
-        <Route path="/Home" element={<Home />} />
+        <Route path="/Wekcadmin" element={<Wekcadmin />} />
+        <Route path="/Classifieds" element={<Classifieds />} />
         <Route path="/ProviderDashboard" element={<ProviderDashboard />} />
         <Route path="/Terms" element={<Terms />} />
-        <Route path="/Classifieds" element={<Classifieds />} />
-        <Route path="/Wekcadmin" element={<Wekcadmin />} />
-        <Route path="/ListService" element={<ListService />} />
         <Route path="/Privacy" element={<Privacy />} />
+        <Route path="/ListService" element={<ListService />} />
+        <Route path="/Home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
